bf681e57aed605cbe8c85ae3038d60ff134fb7bb
Andrew <andrew.mk4@yahoo.com>
2021-10-21 03:53:28 -0700

    update

--- src/components/layout.js
+++ src/components/layout.js
@@ -7,6 +7,7 @@
 
  import * as React from "react"
  import PropTypes from "prop-types"
+//  import { Link } from "gatsby"
  import { useStaticQuery, graphql } from "gatsby"
  import Header from "./header"
  import Footer from "./footer"
